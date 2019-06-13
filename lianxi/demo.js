50ce8ff2089c3d5c9bfcc5a9001822801b7dc9b2
He Dian <356839046@qq.com>
2019-06-13 18:11:01 +0800

    add 07 08 mod lianxi

--- lianxi/demo.js
+++ lianxi/demo.js
@@ -1,27 +1,68 @@
-class LikeButton{
-  constructor(){
-    this.state={
+class Component{
+  constructor(props={}){
+    this.props = props;
+  }
+  setState(state){
+    const oldEl = this.el;
+    this.state = state;
+    this.le = this.renderDOM();
+    if(this.onStateChange){
+      this.onStateChange(oldEl,this.le)
+    }
+  }
+  renderDOM(){
+    this.el = createDOMFromString(this.render());
+    if(this.onClick){
+      this.el.addEventListener('click',this.onClick.bind(this),false)
+    }
+    return this.el;
+  }
+}
+
+const createDOMFromString = function(domString){
+  let div = document.createElement("div");
+  div.innerHTML = domString;
+  return div;
+}
+
+const mount = function(component,container){
+  container.appendChild(component.renderDOM())
+  component.onStateChange = function(oldEl,newEl){
+    container.insertBefore(newEl,oldEl);
+    container.removeChild(oldEl);
+  }
+}
+
+class LikeButton extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
       isLike:false
     }
   }
+  onClick(){
+    this.setState({
+      isLike: !this.state.isLike
+    })
+  }
   render(){
-    this.el = this.createDom(`
-      <button>
-        <span class="like-text">点赞</span>
+    return `
+      <button style="background-color:${this.props.bgColor};">
+        <span style="color:${this.props.color};font-size:${this.props.fontSize};">${this.state.isLike?"取消":"关注"}</span>
       </button>
-    `)
-    this.el.addEventListener("click",()=>{
-      this.changLikeText();
-    },false)
-    return this.el;
+    `
   }
-  changLikeText(){
-    this.state.isLike = !this.state.isLike;
-    this.el.querySelector(".like-text").innerHTML = this.state.isLike ? "取消":"点赞"
+}
+
+class Header extends Component{
+  constructor(props){
+    super(props);
   }
-  createDom(domString){
-    let div = document.createElement("div");
-    div.innerHTML = domString;
-    return div;
+  render(){
+    return `
+      <header>
+        <h1 style="color:${this.props.color}">self React Component</h1>
+      </header>
+    `
   }
 }
